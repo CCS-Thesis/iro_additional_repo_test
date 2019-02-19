847d642215f7923ef15bc44428c08656c881d1b8
bleckjeckkk <danjoaquinchin@gmail.com>
2019-02-19 23:16:05 +0800

    changes to arguments, changes to svm handling

--- svm.py
+++ svm.py
@@ -10,24 +10,20 @@ from sklearn import svm, metrics
 # used for getting project constants
 import constants as c
 
+# used for exporting model
+from joblib import dump
+
 # accepts arguments
 # sys.argv[1] = filepath to the output.csv
-# sys.argv[2] = percentage for training OR output_experiment.csv(optional)
+# sys.argv[2] = percentage for training (optional)
 
-EXPERIMENT = False
 if len(sys.argv) > 1:
     csv_file = sys.argv[1]
     try:
         try:
             TRAIN_PERCENT_IN_DECIMAL = float(sys.argv[2])
         except Exception as iden:
-            TRAIN_PERCENT_IN_DECIMAL = c.TRAIN_PERCENT 
-            try:
-                data_exp = pd.read_csv(sys.argv[2])
-                EXPERIMENT = True
-                TRAIN_PERCENT_IN_DECIMAL = 1
-            except Exception as e:
-                print("Please input the correct .CSV file for experimentation.")
+            TRAIN_PERCENT_IN_DECIMAL = c.TRAIN_PERCENT
         data = pd.read_csv(csv_file)
     except Exception as e:
         print("Please input the correct .CSV file.")
@@ -40,23 +36,19 @@ else:
 print(str(TRAIN_PERCENT_IN_DECIMAL * 100) + "%" + " used for training" )
 print(str(data.shape[0]) + " rows obtained.")
 class_stats = data.groupby('aggressive').size()
-print("Aggressive : " + str(class_stats[0]))
-print("Non-aggressive : " + str(class_stats[1]))
+print("Aggressive : " + str(class_stats[1]))
+print("Non-aggressive : " + str(class_stats[0]))
 print("Total : " + str(class_stats[0] + class_stats[1]))
 
 # number of rows to obtain for training
 train = int(TRAIN_PERCENT_IN_DECIMAL * data.shape[0])
-
 
 # shuffling the data
 data = data.sample(frac=1).reset_index(drop=True)
 print("dataset shuffled")
 
 # making subsets for training set
-if not EXPERIMENT:
-    for_training = data[:train]                                 # first 'train' number of rows
-else:
-    for_training = data                                         # get everything
+for_training = data[:train]                                     # first 'train' number of rows
 
 features = for_training.drop(['name','aggressive'], axis=1)     # dropping name and class
 classes = for_training[['aggressive']]                          # getting only class
@@ -67,44 +59,55 @@ classes = for_training[['aggressive']]                          # getting only c
 print("------------------------------------")
 print("Training Dataset")
 agg = for_training.groupby('aggressive').size()
-print("Aggressive : " + str(agg[0]))
-print("Non-aggressive : " + str(agg[1]))
+print("Aggressive : " + str(agg[1]))
+print("Non-aggressive : " + str(agg[0]))
 print("Total : " + str(agg[0] + agg[1]))
 print("------------------------------------")
 
-# making subsets for testing set
-if not EXPERIMENT:
-    for_testing = data[train:]                                      # rows after index 'train'
-    test_features = for_testing.drop(['name','aggressive'], axis=1) # drop name and class
-    test_classes = for_testing[['aggressive']]                      # get only class
+for_testing = data[train:]                                      # rows after index 'train'
+test_features = for_testing.drop(['name','aggressive'], axis=1) # drop name and class
+test_classes = for_testing[['aggressive']]                      # get only class
 
-    # display how much is aggressive, not aggressive
-    # [informative print statements]
-    print("------------------------------------")
-    print("Testing Dataset")
-    test_agg = for_testing.groupby('aggressive').size()             # groupby to aggregate the number of aggressive/non-aggressive tuples
-    print("aggressive : " + str(test_agg[0]))
-    print("non-aggressive : " + str(test_agg[1]))
-    print("Total : " + str(test_agg[0] + test_agg[1]))
-    print("------------------------------------")
-else:
-    for_testing = data_exp                                          # get everything
-    test_features = for_testing.drop(['name'], axis=1)              # drop name
-
+# display how much is aggressive, not aggressive
+# [informative print statements]
+print("------------------------------------")
+print("Testing Dataset")
+test_agg = for_testing.groupby('aggressive').size()             # groupby to aggregate the number of aggressive/non-aggressive tuples
+print("aggressive : " + str(test_agg[1]))
+print("non-aggressive : " + str(test_agg[0]))
+print("Total : " + str(test_agg[0] + test_agg[1]))
+print("------------------------------------")
 
 # training the model
-svc = svm.SVC(kernel='rbf', C=1.0 ,gamma='auto').fit(features, classes.values.ravel())
+svc = svm.SVC(kernel='linear', C=1.0 ,gamma='auto').fit(features, classes.values.ravel())
+# is the penalty value 
 
 # lets the svm predict classes/values
 pred = svc.predict(test_features)
 
-if not EXPERIMENT:
-    
-    print("Confusion Matrix:\n" + str(metrics.confusion_matrix(test_classes,pred)))
+# maybe put some validation here if we going with validation
+print("Confusion Matrix:\n" + str(metrics.confusion_matrix(test_classes,pred)))
 
-    print("Accuracy: " + str(metrics.accuracy_score(test_classes,pred)))
+print("Accuracy: " + str(metrics.accuracy_score(test_classes,pred)))
+
+print("Export this model? (y/n)")
+choice = str(input()).lower()
+if choice == 'y':
+    print("Exporting model...")
+    dump(svc,'model.joblib')
+
+    # # model testing
+    # svm2 = load('model.joblib')
+    # pred2 = svm2.predict(test_features)
+
+    # print("Confusion Matrix:\n" + str(metrics.confusion_matrix(test_classes,pred2)))
+
+    # print("Accuracy: " + str(metrics.accuracy_score(test_classes,pred2)))
+
+    # print("Is this ok? (y/n)")
+
+    print("Model Exported into model.joblib")
+
 else:
-    print(pred)
-    data_exp['predicted'] = pred
-    print(str(data_exp))
-    data_exp.to_csv('classified.csv')
+    print("Ok")
+    
